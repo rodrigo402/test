32fdf1c16521260fad721730cfe3a996ebf49951
Rodrigo Reynoso <rreynoso@sgfintech.com.ar>
2022-05-17 15:45:58 -0300

    chat-out

--- Desktop/Angular/cv-web/src/app/app.component.ts
+++ Desktop/Angular/cv-web/src/app/app.component.ts
@@ -8,13 +8,12 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Rodrigo Reynoso | cv-web';
-  
+
   constructor() { }
 
   ngOnInit(): void {
-    
-        (function(w,d,u){ var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0); var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h); })(window,document,'https://cdn.bitrix24.es/b20539023/crm/site_button/loader_2_3yc3sx.js');
-    
+
+
   }
 }
 
